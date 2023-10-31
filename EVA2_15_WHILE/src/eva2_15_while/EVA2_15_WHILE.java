8f5d0e5e4e3b54bc3db1a9a474f9d58bde266f7a
reginaob <143568670+reginaob@users.noreply.github.com>
2023-10-30 19:52:15 -0600

    Add files via upload

---
+++ EVA2_15_WHILE/src/eva2_15_while/EVA2_15_WHILE.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_while;
+
+/**
+ *
+ * @author regin
+ */
+public class EVA2_15_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        while(i < 10){ //mientras sea verdad la expresion
+            System.out.println("REGINA!!");
+            i++;
+            
+        }
+    }
+    
+}
