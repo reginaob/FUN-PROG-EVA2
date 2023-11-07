a4bd48ac34cefa94303e2670ddaf67eeb50459f8
reginaob <143568670+reginaob@users.noreply.github.com>
2023-11-06 19:45:47 -0600

    Add files via upload

--- EVA2_22_CICLOS_ANIDADOS/src/eva2_22_ciclos_anidados/EVA2_22_CICLOS_ANIDADOS.java
+++ EVA2_22_CICLOS_ANIDADOS/src/eva2_22_ciclos_anidados/EVA2_22_CICLOS_ANIDADOS.java
@@ -4,6 +4,8 @@
  */
 package eva2_22_ciclos_anidados;
 
+import java.util.Scanner;
+
 /**
  *
  * @author regin
@@ -15,6 +17,18 @@ public class EVA2_22_CICLOS_ANIDADOS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int nume;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Introduce un numero");
+        nume = input.nextInt();
+        
+       for (int i = nume; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+            }
+        }
     }
     
-}
+
