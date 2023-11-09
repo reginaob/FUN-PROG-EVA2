adba272281facd154eaafbfc0f392b01496f39fd
reginaob <143568670+reginaob@users.noreply.github.com>
2023-11-08 19:45:42 -0600

    Add files via upload

---
+++ EVA2_24_ARREGLOS_CICLOS/src/eva2_24_arreglos_ciclos/EVA2_24_ARREGLOS_CICLOS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_24_arreglos_ciclos;
+
+/**
+ *
+ * @author regin
+ */
+public class EVA2_24_ARREGLOS_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int [10];
+               //1er = 0; ultimo = N -1
+        for (int i = 0; i < 10; i++) {
+            arreglo[i] = 10000;
+        }
+         for (int i = 0; i < 10; i++) {
+             System.out.println(arreglo[i] );
+         }
+    }
+    
+}
